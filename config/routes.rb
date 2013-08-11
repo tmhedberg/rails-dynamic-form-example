0204ac7d4ac18f1c1e0b1727ce407528779fa6f5
Taylor Hedberg <t@tmh.cc>
2013-08-11 15:12:33 -0400

    Use backwards-compatible symbol array syntax
    So as not to require Ruby 2.0+.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 RailsDynamicFormExample::Application.routes.draw do
   root 'shirt_styles#index'
 
-  resources :shirt_styles, only: %i[index new create]
+  resources :shirt_styles, only: [:index, :new, :create]
 end
