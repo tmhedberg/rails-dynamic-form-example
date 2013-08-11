45385d6f38641281a9586f484b2b5a050e29954e
Taylor Hedberg <t@tmh.cc>
2013-08-11 04:15:11 -0400

    Shirt style create action

--- app/controllers/shirt_styles_controller.rb
+++ app/controllers/shirt_styles_controller.rb
@@ -1,9 +1,19 @@
 class ShirtStylesController < ApplicationController
+  include ActionView::Helpers::TextHelper
+
   def index
     @styles = ShirtStyle.all
   end
 
   def new; end
 
-  def create; end
+  def create
+    params[:shirt_style].each do |_, style|
+      ShirtStyle.create! style if !(style[:color].blank? && style[:size].blank?)
+    end
+    redirect_to(
+      shirt_styles_path,
+      notice: "Created #{pluralize params[:shirt_style].length, 'style'}"
+    )
+  end
 end
